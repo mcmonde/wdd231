ed74b9058fb961933e6ebaf7bcd3cfd1a86f212f
mcmonde <mcmeliton1889@gmail.com>
2024-11-01 11:21:37 +0800

    discover page

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,7 +1,10 @@
-const navMenu = document.getElementById('navMenu');
-const hamburgerMenu = document.getElementById('hamburgerMenu');
+document.addEventListener('DOMContentLoaded', function () {
+    const navMenu = document.getElementById('navMenu');
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
 
-// Toggle mobile menu
-hamburgerMenu.addEventListener('click', function () {
-    navMenu.classList.toggle('active');
+    if (hamburgerMenu && navMenu) {
+        hamburgerMenu.addEventListener('click', function () {
+            navMenu.classList.toggle('active');
+        });
+    }
 });
